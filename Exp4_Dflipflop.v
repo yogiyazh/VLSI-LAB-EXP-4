c1b98ed60f033610493c007b557d4a274117e343
yogiyazh <159939338+yogiyazh@users.noreply.github.com>
2024-04-15 14:10:21 +0530

    Update Exp4_Dflipflop.v

--- Exp4_Dflipflop.v
+++ Exp4_Dflipflop.v
@@ -1,8 +1,13 @@
-6(iii): D FLIP FLOP 
-module dff( clk,d,q); 
-input clk,d; 
-output reg q; 
-
-
-
+module DFlipFlop (D, clk, reset, Q) ;
+input D;
+input clk;
+input reset; 
+output reg Q; 
+always @ (posedge clk)
+begin
+    if(reset == 1'b1)
+        Q <= 1'b0;
+    else
+        Q <= D;
+end
 endmodule
