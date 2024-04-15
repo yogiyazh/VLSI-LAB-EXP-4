c0a4fbb7c7a16dd5b7c4ad6c6f43bb26bcf01190
yogiyazh <159939338+yogiyazh@users.noreply.github.com>
2024-04-15 14:12:23 +0530

    Update Exp4_SRflipflop.v

--- Exp4_SRflipflop.v
+++ Exp4_SRflipflop.v
@@ -1,13 +1,18 @@
-module sr( s,r,clk,q);
- input s,r,clk; 
-output reg q=0; 
-
-
-
-
+module SR_flipflop (q, q_bar, s,r, clk, reset);
+  input s,r,clk, reset;
+  output reg q;
+  output q_bar;
+  always@(posedge clk) begin 
+    if(!reset)        q <= 0;
+    else 
+  begin
+      case({s,r})
+        2'b00: q <= q;    
+        2'b01: q <= 1'b0; 
+        2'b10: q <= 1'b1; 
+        2'b11: q <= 1'bx; 
+      endcase
+    end
+  end
+  assign q_bar = ~q;
 endmodule
-
-
-
-
-
